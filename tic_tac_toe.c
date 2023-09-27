cb987fadf90ca256aa9365d337ab23ee52feb5b8
moeller-marc <122219863+moeller-marc@users.noreply.github.com>
2023-09-27 09:52:13 +0200

    progress of day 27 cs class

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -5,14 +5,29 @@
 
 #define SIZE 9
 
+typedef struct
+{
+    int *board;
+    int player;
+    int *empty_cells;
+    best_move best_move;
+} minimax_node;
+
+typedef struct
+{
+    int *board;
+    int score;
+} best_move;
+
 void print_board(int *board);
 void set_board(int *board);
 int is_terminal(int *board);
+void get_all_emty_cells(int *board, int *empty_cells);
+void minimax_with_pruning(minimax_node *node);
 
 int main(int argc, char const *argv[])
 {
     int board[SIZE];
-    set_board(board);
     print_board(board);
     board[1] = 2;
     board[4] = 2;
@@ -92,4 +107,38 @@ int is_terminal(int *board)
         }
     }
     return 0;
+}
+
+void get_all_emty_cells(int *board, int *empty_cells)
+{
+    int j = 0;
+    for (int i = 1; i < SIZE + 1; i++)
+    {
+        if (board[i] == 0)
+        {
+            empty_cells[j] = i;
+            j += 1;
+        }
+    }
+    empty_cells[0] = j;
+}
+
+void minimax_with_pruning(minimax_node *node)
+{
+    if (is_terminal(->board) != 0)
+    {
+        if (is_terminal(node->board) == 1)
+        {
+            best_move best_move = {*board, 1};
+            node->best_move = best_move;
+        }
+        else if (is_terminal(node->board) == 2)
+        {
+            node->best_move = -1;
+        }
+        else if (is_terminal(node->board) == 3)
+        {
+            node->best_move = 0;
+        }
+    }
 }
